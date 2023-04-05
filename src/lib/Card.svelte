c294da3608f2f3d473acab4c5812246059e1e533
Jezer Mejía <amyuki4@icloud.com>
2023-04-05 16:40:13 -0600

    Added Card component

---
+++ src/lib/Card.svelte
@@ -0,0 +1,21 @@
+<script lang="ts">
+  export let src: string = "";
+</script>
+
+<div class="card">
+  <img {src} alt="" />
+  <div class="cardContent">
+    <slot />
+  </div>
+</div>
+
+<style>
+  .card {
+    display: flex;
+    flex-direction: column;
+    box-sizing: border-box;
+  }
+  .cardContent {
+    padding: 1.25em;
+  }
+</style>
