e58c7f4250ba270ed606bb930b5f891d749dec24
Jezer Mejía <amyuki4@icloud.com>
2023-04-05 17:16:02 -0600

    Added Vercel analytics

--- src/routes/+layout.ts
+++ src/routes/+layout.ts
@@ -1,7 +1,12 @@
+import { dev } from "$app/environment";
+import { inject } from "@vercel/analytics";
 import type { LayoutLoad } from "./$types";
+
 export const prerender = true;
 
 export const load = (({ url }) => {
   const { pathname } = url;
   return { pathname };
 }) satisfies LayoutLoad;
+
+inject({ mode: dev ? "development" : "production" });
