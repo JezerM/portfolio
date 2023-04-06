18a59f22dbf39812c60c9da3c90a154b6013c128
Jezer Mejía <amyuki4@icloud.com>
2023-04-06 00:03:14 -0600

    Implemented a basic contactMe page

--- src/routes/contact/+page.svelte
+++ src/routes/contact/+page.svelte
@@ -1,1 +1,15 @@
-CONTACT ME
+<div>
+  <h2>Contact me</h2>
+  <p>
+    If you want to hire me, ask me about my projects or anything else, you can contact me through
+    <a href="mailto:amyuki4@icloud.com">Email</a> or Discord.
+  </p>
+</div>
+
+<style>
+  a {
+    text-decoration-line: underline;
+    text-decoration-thickness: 0.1em;
+    color: #83a598;
+  }
+</style>
