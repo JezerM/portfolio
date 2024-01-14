969586b0702e15461bf2fef7e97168e613eb0168
Jezer Mejía <amyuki4@icloud.com>
2024-01-13 20:01:28 -0600

    Yeah... removed code

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,6 @@
 import { defineConfig } from "vite";
 import { sveltekit } from "@sveltejs/kit/vite";
-import { enhancedImages } from "@sveltejs/enhanced-img";
 
 export default defineConfig({
-  plugins: [sveltekit(), enhancedImages()],
+  plugins: [sveltekit()],
 });
