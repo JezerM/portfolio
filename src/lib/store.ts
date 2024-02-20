0ae15f4553d35ebed9a96fb761462d39806e2173
Jezer Mejía <amyuki4@icloud.com>
2024-02-19 21:01:19 -0600

    Format some files

--- src/lib/store.ts
+++ src/lib/store.ts
@@ -2,5 +2,5 @@ export const socialLinks = {
   discord: "http://discordapp.com/users/530819150969438208",
   github: "https://github.com/JezerM",
   linkedin: "https://www.linkedin.com/in/jezer-josué-mejía-otero-111b39227/",
-  email: "mailto:jezer.mejia@icloud.com"
+  email: "mailto:jezer.mejia@icloud.com",
 } as const;
