c005e1963575c3755bc530bfe6a9f3c94d6eaa84
Jezer Mejía <amyuki4@icloud.com>
2025-01-25 22:44:17 -0600

    Remove store.ts file

--- src/lib/store.ts
+++
@@ -1,6 +0,0 @@
-export const socialLinks = {
-  discord: "http://discordapp.com/users/530819150969438208",
-  github: "https://github.com/JezerM",
-  linkedin: "https://www.linkedin.com/in/jezer-josué-mejía-otero-111b39227/",
-  email: "mailto:jezer.mejia@icloud.com",
-} as const;
